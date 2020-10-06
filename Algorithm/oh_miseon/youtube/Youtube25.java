190977b2293bd22de21d98ffb3603e78dbec63e6
ohmiseon <ohmisen710@naver.com>
2020-10-06 22:11:31 +0900

    201006 youtube

---
+++ Algorithm/oh_miseon/youtube/Youtube25.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/*
+	javac Youtube24.java -encoding UTF-8
+	N이 주어졌을 떄 N의 사이클의 길이를 구하시오(1<=N <= 100)
+	
+	26
+	2+6=8 -> 68 
+	6+8=14->84 
+	8+4=12 -> 42 
+	4+2=6 26
+	= 4번
+	
+*/
+class Youtube25 {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		
+	}
+}
